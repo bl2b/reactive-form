f2a57cad0685a6c74cadd81187ad009af0f4845a
Jovenson N. Omagap <Jovenson.Omagap@nowcom.com.ph>
2018-06-08 16:28:22 +0800

    css

--- src/app/components/stock-counter/stock-counter.component.ts
+++ src/app/components/stock-counter/stock-counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stock-counter',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-counter.component.scss']
 })
 export class StockCounterComponent implements OnInit {
+  @Input() step: number = 10;
+  @Input() min: number = 10;
+  @Input() max: number = 1000;
+
+  value: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  increment(): void {
+    if (this.value < this.max) {
+      this.value = this.value + this.step;
+    }
+  }
+
+  decrement(): void {
+    if (this.value > this.min) {
+      this.value = this.value - this.step;
+    }
   }
 
 }
